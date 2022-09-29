e645bd601a619dfddc2ea1b33f19cd4e87e4c527
satyendrasingh8 <satyendrasingh@forcebolt.com>
2022-09-29 14:59:24 +0530

    initial commit mobile web detects metamask

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import Web3 from "web3";
+import { useState } from "react";
 
-function App() {
+import "./App.css";
+import ConnectWalletButton from "./components/ConnectWalletButton";
+import mobileCheck from "./helpers/mobileCheck";
+import getLinker from "./helpers/deepLink";
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [address, setAddress] = useState("");
+
+  const onPressConnect = async () => {
+    setLoading(true);
+
+    try {
+      const yourWebUrl = "localhost:3000"; // Replace with your website domain
+      const deepLink = `https://metamask.app.link/dapp/${yourWebUrl}`;
+      const downloadMetamaskUrl = "https://metamask.io/download.html";
+
+      if (window?.ethereum?.isMetaMask) {
+        // Desktop browser
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        const account = Web3.utils.toChecksumAddress(accounts[0]);
+        setAddress(account);
+      } else if (mobileCheck()) {
+        // Mobile browser
+        const linker = getLinker(downloadMetamaskUrl);
+        linker.openURL(deepLink);
+      } else {
+        window.open(downloadMetamaskUrl);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    setLoading(false);
+  };
+
+  const onPressLogout = () => setAddress("");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ConnectWalletButton
+          onPressConnect={onPressConnect}
+          onPressLogout={onPressLogout}
+          loading={loading}
+          address={address}
+        />
       </header>
     </div>
   );
-}
+};
 
 export default App;
